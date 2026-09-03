6da601fa0e44fd476f3b3f0a44f0ad5265ed11e1
Frankrochha7 <frank.procha@senacsp.edu.br>
2026-09-03 02:35:59 -0300

    Fix boolean expressions and formatting in Exercicio7

--- Exercicio7.java
+++ Exercicio7.java
@@ -1,19 +1,21 @@
+//Frank Rocha
+
 public class Exercicio7 {
     public static void main(String[] args) {
 
-        // Expressão 1
+        
         boolean expressao1 = (120 - 30) == Math.pow(3, 30);
         System.out.println(expressao1);
 
-        // Expressão 2
+       
         boolean expressao2 = !((20 % 4) == 1) || (9 != 9);
         System.out.println(expressao2);
 
-        // Expressão 3
+       
         boolean expressao3 = (5 % 2) > 3;
         System.out.println(expressao3);
 
-        // Expressão 4
+        
         boolean expressao4 = ('a' == 'A');
         System.out.println(expressao4);
     }
