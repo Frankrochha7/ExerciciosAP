7082c8d9f39459429998a4906b0c2e62828e035e
Frankrochha7 <frank.procha@senacsp.edu.br>
2026-09-03 02:21:38 -0300

    Add files via upload

---
+++ Exercicio1.java
@@ -0,0 +1,12 @@
+
+
+public class Exercicio1 {
+    
+public static void main(String[] args) {
+
+System.out.println("Frank Pereira da Rocha");
+System.out.println("6263333");
+System.out.println(" Analise e desenvolvimento de sistemas");
+}
+
+}
